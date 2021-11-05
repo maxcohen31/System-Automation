482fda94550bf971411c46c109cc0c11e91bcf04
maxcohen31 <eman_93@hotmail.it>
2021-11-05 17:43:21 +0100

    adding journal

---
+++ journal_cleaning.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+journalpath='/var/log/journal/'
+action=$(du -sh $journalpath | awk '{print $1}' | cut -d M -f1)
+
+if [ $action -gt 500 ]; then
+  journal --vacumm-time=2weeks
+else
+  echo "Space under 500M"
+
+fi
